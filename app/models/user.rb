094c673f615a031179d1eca4ee7ca0f15b1b1084
yamamotomakiko <yamamoto-makiko@av.avex.co.jp>
2017-02-08 12:33:17 +0000

    実習02

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # サムネイル画像にCarrierWaveで作成したUserThumbnailUploaderを使用
+  mount_uploader :thumbnail, UserThumbnailUploader
+  # バリデーションを設定
+  validates :name, presence: true
+  validates :agreement, presence: true, acceptance: {accept: true}
+end
