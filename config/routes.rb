dc21f5cb5f5cf7a0bd7127c8f16d0704f4923fbb
yamamotomakiko <yamamoto-makiko@av.avex.co.jp>
2017-02-10 13:21:58 +0000

    実習08

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'books#index'
   resources :books
+  resources :bookmarks, only: [:create, :destroy]
   get 'books/index'
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208130156) do
+ActiveRecord::Schema.define(version: 20170210131028) do
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
