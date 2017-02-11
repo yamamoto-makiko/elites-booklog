e016b343c1b76375bb176d5c32d4c7771768de84
yamamotomakiko <yamamoto-makiko@av.avex.co.jp>
2017-02-11 14:50:47 +0000

    実習09

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,8 @@ class BooksController < ApplicationController
 
   def index
     # @books = Book.order('updated_at DESC')
-    @books = Book.includes(:bookmarks).order('updated_at DESC')
+    # @books = Book.includes(:bookmarks).order('updated_at DESC')
+    @books = Book.includes(:bookmarks, :reviews).order('updated_at DESC')
   end
   
   def show
@@ -12,6 +13,14 @@ class BooksController < ApplicationController
     if user_signed_in?
       # 自分のブックマークの選択
       @my_bookmark = @book.bookmarks.select{|s| s.user_id == current_user.id}.first
+    end
+    if user_signed_in?
+      # 自分のレビューの存在確認
+      my_review = @book.reviews.select{|s| s.user_id == current_user.id}.first
+      unless my_review
+        # レビューが無い場合は入力フォームを作成
+        @my_review = Review.new
+      end
     end
   end
 
